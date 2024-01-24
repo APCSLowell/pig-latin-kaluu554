7669e8026c446e8fb813e64a19a81761e3f8a402
kaluu554 <143027760+kaluu554@users.noreply.github.com>
2024-01-23 19:33:24 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -22,9 +22,9 @@ public class PigLatin {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-	    System.out.println("there are " + lines.length + " lines");
-	    for (int i = 0 ; i < lines.length; i++) {
-	        System.out.println(pigLatin(lines[i]));
+	    System.out.println("there are " + lines.length() + " lines");
+	    for (int i = 0 ; i < lines.length(); i++) {
+	        System.out.println(PigLatin(lines[i]));
 	    }
     }
     public int findFirstVowel(String sWord) {
